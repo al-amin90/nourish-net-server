63a1f8acc694676b1547e8ecb97cd67c38bbd947
al-amin90 <ijesun30@gmail.com>
2024-05-30 16:56:02 +0600

    payment and dashboard api added

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken')
-const cookieParser = require('cookie-parser')
-const app = express()
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const app = express();
 
 // config
-require('dotenv').config()
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors({
+app.use(
+  cors({
     origin: [
-        "http://localhost:5173",
-        "https://nourish-net-9ac72.web.app",
-        "https://nourish-net-9ac72.firebaseapp.com"
+      "http://localhost:5173",
+      "https://nourish-net-9ac72.web.app",
+      "https://nourish-net-9ac72.firebaseapp.com",
     ],
     credentials: true,
-
-}))
-app.use(express.json())
-app.use(cookieParser())
-
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.hjmc0vt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -32,167 +32,203 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
-
 
 // middleware
 const tokenVerify = async (req, res, next) => {
-    const token = req.cookies.token;
-    
-    if (!token) {
-        return res.status(401).send("unAuthorize user")
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).send("unAuthorize user");
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send("unAuthorize user");
     }
-    
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-             return res.status(401).send("unAuthorize user")
-        }
 
-        req.user = decoded;
-        next()
-    })
-}
-
+    req.user = decoded;
+    next();
+  });
+};
 
 const cookieOption = {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production"? true: false,
-    sameSite: process.env.NODE_ENV === "production"? "none": "strict",
-}
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production" ? true : false,
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-    
-    const foodsCollection = client.db("nourishNetDB").collection("foods")
-      
-// jwt apis
-      app.post('/jwt', async (req, res) => {
-          const user = req.body;
-       
-          const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" })
-          
-          res
-              .cookie('token', token, cookieOption)
-              .send({success: true})
-      })
-    
+
+    const foodsCollection = client.db("nourishNetDB").collection("foods");
+    const donatesCollections = client.db("nourishNetDB").collection("donate");
+
+    // jwt apis
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+
+      res.cookie("token", token, cookieOption).send({ success: true });
+    });
+
     // jwt logout
-    app.post('/logout', async (req, res) => {
+    app.post("/logout", async (req, res) => {
       const user = req.body;
       console.log("log out user", user);
 
       res
-        .clearCookie('token', {...cookieOption,maxAge: 0})
-        .send({ success: true })
-    })
+        .clearCookie("token", { ...cookieOption, maxAge: 0 })
+        .send({ success: true });
+    });
 
-       //  foods rest apis in home page
-      app.get('/foodsAcc', async (req, res) => {
-        const query = {statusFood: "available"}
-        const result = await foodsCollection.find(query).sort({foodQuantity: -1}).toArray()
+    // payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const price = req.body.price;
+      const priceInCent = parseFloat(price) * 100;
 
-        res.send(result)
-    })
+      console.log(priceInCent);
+      if (!price || priceInCent < 1) return;
+      const { client_secret } = await stripe.paymentIntents.create({
+        amount: priceInCent,
+        currency: "usd",
+        // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+        automatic_payment_methods: {
+          enabled: true,
+        },
+      });
 
+      res.send({ clientSecret: client_secret });
+    });
+    //  foods rest apis in home page
+    app.get("/foodsAcc", async (req, res) => {
+      const query = { statusFood: "available" };
+      const result = await foodsCollection
+        .find(query)
+        .sort({ foodQuantity: -1 })
+        .toArray();
+
+      res.send(result);
+    });
 
     //  foods rest apis
-    app.get('/foods', async (req, res) => {
+    app.get("/foods", async (req, res) => {
       const available = req.query.avi;
       const sort = req.query.sort;
       const search = req.query.search;
-      
 
       let query = {
-        foodName: {$regex: search, $options: "i"},
-      }
-      let options = {}
+        foodName: { $regex: search, $options: "i" },
+      };
+      let options = {};
 
       if (available) {
-        query.statusFood = available
+        query.statusFood = available;
       }
-      if(sort) {
-        options = {sort: {expiredDate: sort === "Ascending Order" ? 1 : -1}}
+      if (sort) {
+        options = {
+          sort: { expiredDate: sort === "Ascending Order" ? 1 : -1 },
+        };
       }
-      
-        const result = await foodsCollection.find(query,options).toArray()
-        res.send(result)
-    })
-    
+
+      const result = await foodsCollection.find(query, options).toArray();
+      res.send(result);
+    });
+
     // get single food
-    app.get('/food/:id', tokenVerify, async (req, res) => {
+    app.get("/food/:id", tokenVerify, async (req, res) => {
       const id = req.params.id;
 
-      const result = await foodsCollection.findOne({ _id: new ObjectId(id) })
-      res.send(result)
-    })
+      const result = await foodsCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
 
     // add food manage
-      app.get('/foodss', tokenVerify, async (req, res) => {
-        let query = {}
-        const request = req.query.requ;
-        console.log(request);
+    app.get("/foodss", tokenVerify, async (req, res) => {
+      let query = {};
+      const request = req.query.requ;
+      console.log(request);
 
-        const user = req.user.email;
-        const email = req.query.email;
+      const user = req.user.email;
+      const email = req.query.email;
 
-        // verifying-------
-        if (email !== user) {
-            return res.status(403).send('Forbidden Access')
-        }
-        
-        if (request) {
-          query.statusFood = request;
-          query['userEmail'] = email;
-        }
-        else {
-          query = { 'donateUser.email': email }
-        }
-      
-          
-          const result = await foodsCollection.find(query).toArray()
-          res.send(result)
-      })
-      
+      // verifying-------
+      if (email !== user) {
+        return res.status(403).send("Forbidden Access");
+      }
+
+      if (request) {
+        query.statusFood = request;
+        query["userEmail"] = email;
+      } else {
+        query = { "donateUser.email": email };
+      }
+
+      const result = await foodsCollection.find(query).toArray();
+      res.send(result);
+    });
+
     //  post food
-    app.post('/foods',tokenVerify, async (req, res) => {  
-        const food = req.body;
+    app.post("/foods", tokenVerify, async (req, res) => {
+      const food = req.body;
       const token = req.cookies.token;
 
-      const result = await foodsCollection.insertOne(food)
-      res.send(result)
-    })
+      const result = await foodsCollection.insertOne(food);
+      res.send(result);
+    });
 
     // update food
-    app.put('/food/:id', tokenVerify, async (req, res) => {
+    app.put("/food/:id", tokenVerify, async (req, res) => {
       const id = req.params.id;
       const food = req.body;
 
-      const options = {upsert: true}
-      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
-          ...food
-        }
-      }
-      const result = await foodsCollection.updateOne(filter, updateDoc,options);
-      res.send(result)
-    })
-      
+          ...food,
+        },
+      };
+      const result = await foodsCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+
     // delete single food
-    app.delete('/food/:id', tokenVerify, async (req, res) => {
+    app.delete("/food/:id", tokenVerify, async (req, res) => {
       const id = req.params.id;
 
-      const result = await foodsCollection.deleteOne({ _id: new ObjectId(id) })
-      res.send(result)
-    })
+      const result = await foodsCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
 
+    // booking save a room data in db
+    app.post("/donate", tokenVerify, async (req, res) => {
+      const donatesData = req.body;
+      const result = await donatesCollections.insertOne(donatesData);
+      res.send(result);
+    });
+
+    app.get("/donate/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await donatesCollections.find({ email: email }).toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -200,13 +236,11 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
 // --------end part
-app.get('/', (req, res) => {
-    res.send("nourishNet server running perfectlly...")
-})
+app.get("/", (req, res) => {
+  res.send("nourishNet server running perfectlly...");
+});
 
 app.listen(port, () => {
-    console.log(`server is running on port ${port}`)
-})
+  console.log(`server is running on port ${port}`);
+});
